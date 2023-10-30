d18f78f80c685d70824f99e5772da1a6902e4064
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-10-30 15:41:17 +0000

    socket test

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(new URL("./index.html", import.meta.url).pathname);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
